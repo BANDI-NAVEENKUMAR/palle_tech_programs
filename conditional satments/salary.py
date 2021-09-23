cc788c5476466eb9cdc6ad8bb3cbfaaecfd77650
Bandi Naveenkumar <stylishnaveen17@gmail.com>
2021-09-23 12:19:00 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ conditional satments/salary.py
@@ -0,0 +1,6 @@
+salary = 400
+if salary > 500000:
+    salary = salary - (10/100*salary)
+else:
+    salary = salary - (5/100*salary)
+print(salary)
---
+++ some eg pro/b.py
@@ -0,0 +1,6 @@
+salary = 300
+if salary > 500000:
+    salary = salary - (10/100*salary)
+else:
+    salary = salary - (5/100*salary)
+print(salary)
